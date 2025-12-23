1d04a806f79253bd79ea9d722cc0bbbad9917dc4
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-12-23 18:05:15 +0800

    [Update] Add property chunkSize and propRules to setting

--- src/store/setting.ts
+++ src/store/setting.ts
@@ -8,4 +8,19 @@ export const setting = atom({
     uiOffsetH: 0,
     saveSlot: 5,
     tileWidth: 48,
+    chunkSize: 16,
+    propRules: {
+        pot: {
+            density: 0.03,
+            min: 0,
+            max: 6,
+        },
+        chest: {
+            perRoomChance: 0.25,
+            maxPerRoom: 1,
+        },
+        shrine: {
+            perFloorChance: 0.1,
+        }
+    }
 })
