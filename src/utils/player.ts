359f893b0965ec0f01eb4ec7536b22e7b388bbb2
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-12-26 18:25:02 +0800

    [Update] Move camera related function to its own file

--- src/utils/player.ts
+++ src/utils/player.ts
@@ -1,24 +1,22 @@
 import k from '../lib/kaplay'
-import type { GameObj, Vec2 } from "kaplay";
+import type { GameObj} from "kaplay";
+
+import { setCameraPosition } from './camera';
 
 // Store
-import { createStore } from 'jotai'
-import { setting } from '../store/setting';
-const store = createStore()
-
-const chunkMargin = 5
+// import { createStore } from 'jotai'
+// import { setting } from '../store/setting';
+// const store = createStore()
 
 const {
     area,
     body,
     getData,
-    getCamPos,
     isKeyDown,
     pos,
     Rect,
     setData,
     sprite,
-    setCamPos,
     vec2
 } = k
 
@@ -44,7 +42,7 @@ export const createPlayerSprite = (map: GameObj, x: number, y: number, mapWidth:
         pos(0, player.height),
     ])
 
-    setCameraPosition(map, player, mapWidth, mapHeight)
+    setCameraPosition(player, mapWidth, mapHeight)
 
     // Enable control
     setData('ready', true)    
@@ -59,7 +57,7 @@ export const createPlayerSprite = (map: GameObj, x: number, y: number, mapWidth:
         }        
 
         if (isKeyDown("left") && !isKeyDown([ "right", "up", "down" ])){
-            setCameraPosition(map, player, mapWidth, mapHeight)
+            setCameraPosition(player, mapWidth, mapHeight)
             if(player.getCurAnim()?.name !== 'walk') player.play("walk")
             player.flipX = false
 
@@ -73,7 +71,7 @@ export const createPlayerSprite = (map: GameObj, x: number, y: number, mapWidth:
         }
         
         if (isKeyDown("right") && !isKeyDown([ "left", "up", "down" ])){
-            setCameraPosition(map, player, mapWidth, mapHeight)
+            setCameraPosition(player, mapWidth, mapHeight)
             if(player.getCurAnim()?.name !== 'walk') player.play("walk")
             player.flipX = true
 
@@ -87,7 +85,7 @@ export const createPlayerSprite = (map: GameObj, x: number, y: number, mapWidth:
         }        
 
         if (isKeyDown("up") && !isKeyDown([ "right", "left", "down" ])){
-            setCameraPosition(map, player, mapWidth, mapHeight)
+            setCameraPosition(player, mapWidth, mapHeight)
             if(player.getCurAnim()?.name !== 'walk') player.play("walk")
 
             const wPos = player.worldPos()
@@ -100,7 +98,7 @@ export const createPlayerSprite = (map: GameObj, x: number, y: number, mapWidth:
         }     
         
         if (isKeyDown("down") && !isKeyDown([ "right", "up", "left" ])){
-            setCameraPosition(map, player, mapWidth, mapHeight)
+            setCameraPosition(player, mapWidth, mapHeight)
             if(player.getCurAnim()?.name !== 'walk') player.play("walk")
 
             const wPos = player.worldPos()
@@ -114,170 +112,3 @@ export const createPlayerSprite = (map: GameObj, x: number, y: number, mapWidth:
     })  
     // #endregion  
 }
-
-// #region Camera position
-const setCameraPosition = (map: GameObj, player: GameObj, mapWidth: number, mapHeight: number) => {
-    // Decide to move the camera or not
-    const { width, height } = store.get(setting)
-    const middleX = width / 2 
-    const middleY = height / 2 
-
-    const wPos = player.worldPos()
-    let inX = false, inY = false;
-
-    console.log(wPos)
-
-    // Player pos relative to the game world
-    if((wPos.x + middleX) <= mapWidth && (wPos.x - middleX) >= 0){ 
-        inX = true
-    }
-
-    if((wPos.y - middleY) >= 0 && (wPos.y + middleY) <= mapHeight){ 
-        inY = true
-    }
-    
-    // Camera follows player
-    if(inX && inY){
-        console.log('camera follows player')
-        setCamPos(player.pos)
-        getCameraEdges('middle')
-    }
-        
-    if(inX && !inY){
-        // Reached top?
-        if((wPos.y - middleY) <= 0){
-            console.log('camera top')
-            setCamPos(wPos.x, middleY)
-            getCameraEdges('top')
-        }
-
-        // Reached down?
-        if((wPos.y + middleY) >= mapHeight){
-            console.log('camera down')
-            setCamPos(wPos.x, mapHeight - middleY)
-            getCameraEdges('down')
-        }
-    }
-
-    if(!inX && inY){
-        // Reached right?
-        if((wPos.x + middleX) >= mapWidth){
-            console.log('camera right')
-            setCamPos(mapWidth - middleX, wPos.y)
-            getCameraEdges('right')
-        }
-
-        // Reached left?
-        if((wPos.x - middleX) <= 0){
-            console.log('camera left')
-            setCamPos(middleX, wPos.y)
-            getCameraEdges('left')
-        }
-    }
-
-    if(!inX && !inY){
-        // Reached top right?
-        if((wPos.y - middleY) <= 0 && (wPos.x + middleX) >= mapWidth){
-            console.log('camera top right')
-            setCamPos(mapWidth - middleX, middleY)
-            getCameraEdges('topRight')
-        }
-
-        // Reached down right?
-        if((wPos.y + middleY) >= mapHeight && (wPos.x + middleX) >= mapWidth){
-            console.log('camera down right')
-            setCamPos(mapWidth - middleX, mapHeight - middleY)
-            getCameraEdges('downRight')
-        }
-
-        // Reached down left?
-        if((wPos.y + middleY) >= mapHeight && (wPos.x - middleX) <= 0){
-            console.log('camera down left')
-            setCamPos(middleX, mapHeight - middleY)
-            getCameraEdges('downLeft')
-        }
-        
-        // Reached top left?
-        if((wPos.y - middleY) <= 0 && (wPos.x - middleX) <= 0){
-            console.log('camera top left')
-            setCamPos(middleX, middleY)
-            getCameraEdges('topLeft')
-        }
-    }
-}
-// #endregion    
-
-// #region Active / Deactive chunks
-const getCameraEdges = (direction: string) => {
-    // Get the distance between the player and the camera edge
-    const { width, height, tileWidth } = store.get(setting)
-    const cPos = getCamPos()
-    const halfWidth = width / 2
-    const halfHeight = height / 2
-
-    switch(direction){
-        case 'top':{
-            const top = Math.floor(0 / tileWidth)
-            const down = Math.floor((0 + halfHeight) / tileWidth)
-            const left = Math.floor((cPos.x - halfWidth) / tileWidth)
-            const right = Math.floor((cPos.x + halfWidth) / tileWidth)
-
-            updateChunks({ top, down, left, right })
-        }
-        break;
-        case 'down':{
-            //
-        }
-        break;
-        case 'left':{
-            //
-        }
-        break;
-        case 'right':{
-            //
-        }
-        break;
-        case 'middle':{
-            //
-        }
-        break;        
-        case 'topLeft':{
-            //
-        }
-        break;
-        case 'topRight':{
-            //
-        }
-        break;
-        case 'downLeft':{
-            //
-        }
-        break;
-        case 'downRight':{
-            //
-        }
-        break;        
-    }    
-}
-
-const updateChunks = (camera: {top: number, down: number, left: number, right: number}) => {
-    // Get the distance between the player and the camera edge
-    const { chunkSize } = store.get(setting)
-
-    const { top, down, left, right } = camera
-
-    const cT = Math.floor(top / chunkSize) - chunkMargin
-    const cD = Math.floor(down / chunkSize) + chunkMargin
-    const cL = Math.floor(left / chunkSize) - chunkMargin
-    const cR = Math.floor(right / chunkSize) + chunkMargin   
-    
-    const needed = new Set()
-
-    for (let cy = cT; cy <= cD; cy++) {
-        for (let cx = cL; cx <= cR; cx++) {
-            needed.add(`${cx},${cy}`)
-        // activateChunk(cx, cy)
-        }
-    }    
-}
-// #endregion
