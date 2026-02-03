0b7813f3270714bc14cfbc860b2ba6b45715f4dd
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-02-03 17:12:10 +0800

    [WIP] Destory dropped item on the map when player pick up the item

--- src/utils/item.ts
+++ src/utils/item.ts
@@ -1,13 +1,17 @@
-import potData from '../data/pot.json'
-import chestData from '../data/chest.json'
 import potionData from '../data/potion.json'
 import otherData from '../data/other.json'  
-// import cardData from '../data/card.json'
+import cardData from '../data/card.json'
+import headData from '../data/head.json'
+import handData from '../data/hand.json'
+import bodyData from '../data/body.json'
+import feetData from '../data/feet.json'
+import accessoryData from '../data/accessory.json'
 
 import type { GameObj, Vec2 } from 'kaplay'
 import k from '../lib/kaplay'
-import { getGameStoreValue } from '../store/game'
+import { gameStore, getGameStoreValue, inventoryUI } from '../store/game'
 import { getOptionValue } from '../store/setting'
+import type { base, note } from '../model/item'
 
 const { 
     area,
@@ -19,9 +23,10 @@ const {
     // normalizedCurve,
     // loop,
     pos,
-    rect,
+    // rect,
+    RNG,
     sprite,
-    text,
+    // text,
     tween,
     vec2,
     // wait,
@@ -50,42 +55,67 @@ const continuousTween = (sequence: Vec2[]|number[], obj: GameObj, target: string
     })
 }
 
-export const dropItem = (obj: GameObj, type: string) => {
+export const dropItem = (obj: GameObj, base: base = { 
+    count: { min: 1, max: 1 }, 
+    item: { gold: 0.5 }, 
+    gold: { min: 0, max: 0 } 
+}) => {
     const items = []
-    const { 
-        base 
-    } = type === 'pot'? potData :
-        type === 'chest'? chestData :
-        // type === 'card'? cardData :
-        { base: { count: { min: 1, max: 1 }, item: { gold: 0.5 }, gold: { min: 0, max: 0 } } }      
     
     // Decide how many items to drop
     const count = Math.floor(Math.random() * (base.count.max - base.count.min + 1)) + base.count.min
 
     // Dice roll
-    const rng = Math.random()
+    const rng = new RNG(Date.now()).gen()
 
     for(const [key, value] of Object.entries(base.item)){
         if(rng < value && items.length < count){
             switch(key){
                 case 'gold':{
-                    const goldAmount = Math.floor(Math.random() * (base.gold.max - base.gold.min + 1)) + base.gold.min
+                    const goldAmount = Math.floor(rng * (base.gold.max - base.gold.min + 1)) + base.gold.min
                     items.push({ name: 'gold', amount: goldAmount, frame: 0 })
                 }
                 break;
                 case 'potion':{
                     // Randomly select a potion from potionData
-                    const potionIndex = Math.floor(Math.random() * (potionData.length - 1))
-                    const potion = potionData[potionIndex]
-                    items.push({ name: 'potion', item: potion, frame: 1 })
+                    const potionIndex = Math.floor(rng * potionData.length)
+                    items.push({ name: 'potion', item: potionData[potionIndex], frame: 1 })
                 }
                 break;
                 case 'other':{
                     // Randomly select an other item from otherData
-                    const otherIndex = Math.floor(Math.random() * (otherData.length - 1))
-                    const other = otherData[otherIndex]
-                    items.push({ name: 'other', item: other, frame: 2 })
+                    const otherIndex = Math.floor(rng * otherData.length)
+                    items.push({ name: 'other', item: otherData[otherIndex], frame: 2 })
                 }
+                break;
+                case 'card':{
+                    const cardIndex = Math.floor(rng * cardData.length)
+                    items.push({ name: 'other', item: cardData[cardIndex], frame: 0 })
+                }
+                break;
+                default:{
+                    let equipment
+                    switch(key){
+                        case 'head':
+                            equipment = headData[Math.floor(rng * headData.length)]
+                        break;
+                        case 'body':
+                            equipment = bodyData[Math.floor(rng * bodyData.length)]
+                        break;
+                        case 'hand':
+                            equipment = handData[Math.floor(rng * handData.length)]
+                        break;
+                        case 'feet':
+                            equipment = feetData[Math.floor(rng * feetData.length)]
+                        break;
+                        case 'accessory':
+                            equipment = accessoryData[Math.floor(rng * accessoryData.length)]
+                        break;
+                    }
+                    // TODO: Modify item     
+                    
+                    items.push({ name: key, item: equipment, frame: 0 })
+                }              
                 break;
                 // case 'card':{
                 //     // Randomly select a card from cardData
@@ -108,8 +138,8 @@ export const dropItem = (obj: GameObj, type: string) => {
     const dropY = { start: obj.pos.y - range, end: obj.pos.y + obj.width + range }    
     
     const currentPos = {
-        x: obj.pos.x / tileWidth,
-        y: obj.pos.y / tileWidth
+        x: Math.floor(obj.pos.x / tileWidth),
+        y: Math.floor(obj.pos.y / tileWidth)
     }
 
     // Top
@@ -188,9 +218,50 @@ export const dropItem = (obj: GameObj, type: string) => {
                     0.04,
                 )
 
+                dropped.onClick(() => {
+                    const player = get('player')[0]
+                    const dist = dropped.pos.dist(player.pos)
+                    
+                    if(dist <= 50){
+                        // Push item to inventory
+                        if(item.name !== 'gold' && item.item !== undefined){
+                            const { inventory } = getGameStoreValue()
+                            let availableIndex = -1
+
+                            for(let i=0; i < inventory.limit; i++){
+                                if(inventory.space[i] === undefined){
+                                    availableIndex = i
+                                    inventory.space.push({
+                                        index: i,
+                                        item: item.item
+                                    })
+                                    break
+                                }
+                            }
+                            
+                            if(availableIndex < 0){
+                                // No more space
+                            }else{
+                                // Destory item on the map
+                                dropped.destroy()
+                                gameStore.set(inventoryUI, inventory)
+                            }                            
+                        }else{
+                            // Increase number of gold
+                            const { inventory } = getGameStoreValue()
+                            inventory.gold += item.amount?? 0
+
+                            gameStore.set(inventoryUI, inventory)
+                        }
+                    }else{
+                        // Find a path to the item
+                        player.path = player.navigateTo(dropped.pos)
+                    }
+                })
+
             } catch (error) {
                 console.warn('item object create error', error)
             }
         }
-    })    
+    })
 }
