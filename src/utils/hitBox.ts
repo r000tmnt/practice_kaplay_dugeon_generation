b2f0a1cc12c1fdd51695434c72c692199987c617
Parkcorner <rick.t.ryahoocomtw653@gmail.com>
2026-02-25 15:33:14 +0800

    [FIX] Do not let hp go below 0

--- src/utils/hitBox.ts
+++ src/utils/hitBox.ts
@@ -25,13 +25,16 @@ const {
 const onHitEvent = (hitBox: GameObj, attacker: GameObj, defender: GameObj) => {
     // Calculate damage
     if(hitBox.anim === 'attack'){
+        if(defender.defeat || defender.hp <= 0) return
+
         const { hit, dmg, crit } = calculateDamage(attacker, defender)
         const textHolder = defender.get('text')[0]
 
         if(textHolder){
             if(hit){
-                defender.hp -= dmg
-                defender.attribute.hp -= dmg
+                // Do not let hp go below 0
+                defender.hp -= (dmg > defender.hp)? defender.hp : dmg
+                defender.attribute.hp -= (dmg > defender.attribute.hp)? defender.attribute.hp : dmg
                 // Display dmg number
                 if(crit){
                     textHolder.text = `[yellow]${dmg}[/yellow]`
@@ -43,17 +46,25 @@ const onHitEvent = (hitBox: GameObj, attacker: GameObj, defender: GameObj) => {
                 textHolder.text = "MISS"
             }                 
             
-            // Animate text scale number
+            // Animate text pos and opacity
             tween(
-                textHolder.textTransform.scale,
-                2,
+                textHolder.pos.y,
+                textHolder.pos.y - 10,
                 1,
-                (scale) => textHolder.textTransform.scale = scale
+                (pos) => textHolder.pos.y = pos
             ).onEnd(() => {
-                textHolder.textTransform.scale = 1
                 textHolder.text= ''
                 if(crit) textHolder.size -= textHolder.size / 1.5 
-            })                    
+            })   
+            
+            tween(
+                textHolder.opacity,
+                0,
+                1,
+                (v) => textHolder.opacity = v
+            ).onEnd(() => {
+                textHolder.opacity = 1
+            })               
         }
     }    
 }
