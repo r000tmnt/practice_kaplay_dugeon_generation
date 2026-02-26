0853ed206cabdd138b6760cacdd98d9486f5dfe7
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-02-26 20:46:09 +0800

    [Update] Calculate exp starting value with player lv.

--- src/components/gauge.ts
+++ src/components/gauge.ts
@@ -214,8 +214,8 @@ export const ringGauge = (ringGaugeOption: ringGaugeOption) => {
 
     ring.onDraw(() => {
         const { unit, attribute } = reference
-        const start = unit.max[attribute] / 2.5
-        const ringLength = (start > unit[attribute])? unit.max[attribute] - unit[attribute] : unit.max[attribute] - start 
+        const start = Math.floor(unit.max[attribute] / 2.5) * (unit.lv - 1)
+        const ringLength = unit.max[attribute] - start 
         const progress = (unit[attribute] - start) / ringLength
         const steps = 60
         const thickness = 10
